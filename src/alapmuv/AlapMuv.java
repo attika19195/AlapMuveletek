ad7b8bb6140611a2af8d38977e2400e81ba25b37
ForGetASD <vitayz60@gmail.com>
2022-03-10 14:56:30 +0100

    statisztika 0.9

--- src/alapmuv/AlapMuv.java
+++ src/alapmuv/AlapMuv.java
@@ -2,17 +2,29 @@
 package alapmuv;
 
 public class AlapMuv {
-    static int joValasz=0;
-    static int rosszValasz=0;
-    public static void statisztika(int a){
-        
-    
+    static int joValasz=10;
+    static int rosszValasz=4;
+    public static void statisztika(){
+        String jatekosNev = "Marikanéni";
+        int osszesValasz = joValasz + rosszValasz;
+        double szazalek = (rosszValasz / joValasz) * 100;
+        String atment = "Gratulálok elértél egy eléggé jó százalékot, szerintem nem szükséges további gyakorlás";
+        if (szazalek < 80.0) {
+            atment = "Sajnos ez még nem lesz elég, gyakorolj még egy picit.";
+        }
+        System.out.println("------------------------------------------------------------");
+        System.out.printf("%s játékos statisztikái:\n", jatekosNev);
+        System.out.printf("Az összes kérdés amit megpróbáltál megválaszolni:\t%d\n", osszesValasz);
+        System.out.printf("\t Ebből jó válaszok száma:\t%d\n", joValasz);
+        System.out.printf("\t Ebből rossz válaszok száma:\t%d\n", rosszValasz);
+        System.out.printf("\t\t És az elért százalékod: %.1f\n", szazalek);
+        System.out.println("------------------------------------------------------------");
     }
     private static void muvelet(int muveletSzam) {
         
     }
     
     public static void main(String[] args) {
-        statisztika(1);
+        statisztika();
     } 
 }
