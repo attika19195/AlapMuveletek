520b5cb30e7f2c8a0f94df2b805886f3ad74ff3c
rohacsidana <rohacsi.dana@gmail.com>
2022-03-10 14:46:09 +0100

    osszeadás művelet függvényben + statikus változók

--- src/alapmuv/AlapMuv.java
+++ src/alapmuv/AlapMuv.java
@@ -1,15 +1,34 @@
-
 package alapmuv;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class AlapMuv {
-    static int joValasz=0;
-    static int rosszValasz=0;
-    public static void main(String[] args) {
-        String pulloldle; 
-    }
+
+    static int joValasz = 0;
+    static int rosszValasz = 0;
+    static Random rnd = new Random();
+    static Scanner sc = new Scanner(System.in);
+    static int a, b, valasz;
 
     private static void muvelet(int muveletSzam) {
-        
+        int a, b, valasz;
+
+        if (muveletSzam == 1) {
+            a = rnd.nextInt(100) + 1;
+            b = rnd.nextInt(100) + 1;
+            System.out.printf("%d + %d = \n", a, b);
+            valasz = sc.nextInt();
+            if (valasz == a + b) {
+                joValasz++;
+            } else {
+                rosszValasz++;
+            }
+        }
     }
-    
+
+    public static void main(String[] args) {
+        String pulloldle;
+    }
+
 }
