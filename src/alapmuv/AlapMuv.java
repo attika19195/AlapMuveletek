68a0c4d6a0fd23af5a7bfd2c1555f1639aba74a1
rohacsidana <rohacsi.dana@gmail.com>
2022-03-10 15:34:07 +0100

    szépítgetések

--- src/alapmuv/AlapMuv.java
+++ src/alapmuv/AlapMuv.java
@@ -12,6 +12,38 @@ public class AlapMuv {
     static Scanner sc = new Scanner(System.in);
     static int a, b, valasz;
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Szia! Hogy hívnak? ");
+
+        jatekosNev = sc.nextLine();
+        System.out.println("Üdvözöllek kedves " + jatekosNev);
+
+        System.out.println("Ez a játék segít neked megtanulni a matematika 4 alap műveletét");
+        boolean ujKor = true;
+
+        do {
+            System.out.print("Milyen műveletet szeretnél gyakorolni? [1] összeadás, [2] kivonás, [3] szorzás, [4] osztás: ");
+            int jatekszama = sc.nextInt();
+            if (jatekszama == 1) {
+                System.out.println("Kezdődjön az összeadás!");
+            } else if (jatekszama == 2) {
+                System.out.println("Indulhat a kivonás!");
+            } else if (jatekszama == 3) {
+                System.out.println("Szorzás! Egyszerű mint az egyszer egy!");
+            } else {
+                System.out.println("Indulhat az osztás!");
+            }
+            muvelet(jatekszama);
+
+            System.out.print("Szeretnél még egy kört? (I/N): ");
+            String valasz = sc.nextLine();
+            if (valasz == "N") {
+                ujKor = false;
+            }
+        } while (ujKor);
+    }
+
     public static void statisztika() {
         String jatekosNev = "Marikanéni";
         int osszesValasz = joValasz + rosszValasz;
@@ -113,38 +145,6 @@ public class AlapMuv {
         }
         return false;
 
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Szia! Hogy hívnak? ");
-
-        jatekosNev = sc.nextLine();
-        System.out.println("Üdvözöllek kedves " + jatekosNev);
-
-        System.out.println("Ez a játék segít neked megtanulni a matematika 4 alap műveletét");
-        boolean ujKor = true;
-
-        do {
-            System.out.print("Milyen műveletet szeretnél gyakorolni? [1] összeadás, [2] kivonás, [3] szorzás, [4] osztás: ");
-            int jatekszama = sc.nextInt();
-            if (jatekszama == 1) {
-                System.out.println("Kezdődjön az összeadás!");
-            } else if (jatekszama == 2) {
-                System.out.println("Indulhat a kivonás!");
-            } else if (jatekszama == 3) {
-                System.out.println("Szorzás! Egyszerű mint az egyszer egy!");
-            } else {
-                System.out.println("Indulhat az osztás!");
-            }
-            muvelet(jatekszama);
-
-            System.out.print("Szeretnél még egy kört? (I/N): ");
-            String valasz = sc.nextLine();
-            if (valasz == "N") {
-                ujKor = false;
-            }
-        } while (ujKor);
     }
 
     public static boolean Nagyobb_E_Mint_100_Szorzasnal(int a, int b) {
