c861566d9971d1e6c48e6ff99d9b7c04f03a45af
rohacsidana <rohacsi.dana@gmail.com>
2022-03-10 14:25:03 +0100

    változók deklarálása

--- src/alapmuv/AlapMuv.java
+++ src/alapmuv/AlapMuv.java
@@ -2,9 +2,14 @@
 package alapmuv;
 
 public class AlapMuv {
-
+    static int joValasz=0;
+    static int rosszValasz=0;
     public static void main(String[] args) {
         String pulloldle; 
+    }
+
+    private static void muvelet(int muveletSzam) {
+        
     }
     
 }
